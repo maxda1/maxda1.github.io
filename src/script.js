fa8f0fd3c6e80421f54f12c32715e3a2039bc092
Max Dai <maxdai@umich.edu>
2025-11-04 18:12:56 -0500

    Made site use table instead

---
+++ src/script.js
@@ -0,0 +1,125 @@
+let names = {
+    "Emily Hu":1,
+    "Steve Dai":1,
+    "Ningbo Dai":1,
+    "Songgui Leng":1,
+    "Mengbing Dai":1,
+    "Joanna Ruan":1,
+    "Siqi Dai":1, 
+    "Siyu Dai":1, 
+    "Yongjing Wang":1,
+    "Desheng Sun":1,
+    "Lingli He":1,
+    "Guifang Zheng":1,
+    "Jie Cui":2,
+    "Meisen Li":2,
+    "Songli Niu":2,
+    "Shubin Yuan":2,
+    "Tiecheng Niu":2,
+    "Lili":2,
+    "Yijun Cheng":2,
+    "Cynthia Cheng":2,
+    "Yuhan Sun":3,
+    "Andrea Agas":3,
+    "Steven Messina":3,
+    "Jessie Zhao":3,
+    "Jason Jiang":3,
+    "Sally Chun":3,
+    "Alex Chun":3,
+    "Max Dai":3,
+    "Kaiyang Sun":3,
+    "Alex Rickard":4,
+    "Rebecca Malley":4,
+    "Anita Wong":4,
+    "Will Carmona":4,
+    "Lily Mei":4,
+    "Alan Wu":4,
+    "Weywey Su":4,
+    "Tiffany Chu":4,
+    "Kevin Zhou":4,
+    "Kenneth Li":4,
+    "Charles Yu":4,
+    "Jessica Hu":5,
+    "Tim Lee":5,
+    "Sophia Lee":5,
+    "Angela Chau":5,
+    "Peter Chau":5,
+    "Josiah Chau":5,
+    "Dylan Mcnulty":5,
+    "Elaine Sung":5,
+    "Wilson Chan":5,
+    "Janise Park":5,
+    "Annie Kim":5,
+    "Tiffany Pan":5,
+    "Jordan Zhu":6,
+    "Andrea Zhu":6,
+    "Abigail Zhu":6,
+    "Jeremy Zhu":6,
+    "Christian Zhu":6, 
+    "Jasmine Zhu":6,
+    "Kevin Thompson":6,
+    "Zachary Thompson":6,
+    "Joshua Thompson":6,
+    "Jennifer Chu":6,
+    "YeeCheng Low":6,
+    "Weizhen Zhu":7,
+    "Yongjiao Li":7,
+    "Bak Lok Chu":7,
+    "Ai Zhu Zhang":7,
+    "Debbie Lam":7,
+    "Ching lam":7,
+    "Jonathan Lam":7,
+    "Vicki Lam":7,
+    "Harrison Lam":7,
+    "Elizabeth Lam":7,
+    "Jeffrey Zhu":7,
+    "Linda Huang":7,
+};
+
+const suggestions = document.querySelector(".suggestions");
+const info = document.querySelector(".info");
+const inp = document.getElementById("table-search");
+
+inp.onkeyup = function() {
+    let query = inp.value.toLowerCase();
+    let row = document.getElementById("tableBody").getElementsByTagName("tr");
+    
+    for (var i = 0; i < row.length; i++) {
+        var td = row[i].getElementsByTagName("td")[0];
+        if (td) {
+            n = td.textContent || td.innerText;
+            if (n.toLowerCase().indexOf(query) > -1) {
+                row[i].style.display = "";
+            }
+            else {
+                row[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function populateTable() {
+    const tableBody = document.getElementById("tableBody");
+    const k = Object.keys(names);
+    const namesSorted = {};
+
+    k.sort();
+
+    for (const i of k) {
+        namesSorted[i] = names[i];
+    }
+
+
+    Object.keys(namesSorted).forEach(key => {
+        let row = tableBody.insertRow(); // Create a new table row
+        let name = row.insertCell(0);   // Create the first cell
+        let tableNum = row.insertCell(1);   // Create the second cell
+
+        row.className = "bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600";
+        name.className = "px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        tableNum.className = "px-6 py-4"
+
+        name.innerHTML = key;     // Set content for the first cell
+        tableNum.innerHTML = names[key];     // Set content for the second cell
+    });
+}
